c63b80d8f93d52a05ff60af24ff1280c99b94397
adrien <adrien.bony38@gmail.com>
2024-10-17 09:59:23 +0200

    Routes products

--- app/internal/models/category.go
+++ app/internal/models/category.go
@@ -11,5 +11,5 @@ type Category struct {
 	PictureUrl  string    `gorm:"not null"`
 	Description string    `gorm:"not null"`
 	CategoryID  *uint     `gorm:"default:null"`
-	Category    *Category `gorm:"foreignKey:CategoryID"`
+	Category    *Category `gorm:"foreignKey:CategoryID;nullable"`
 }
